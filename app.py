2aedc6921f364bf83675f905b0bcc289153b7eb1
Kamble, Nikhil <nkamble@iu.edu>
2023-02-23 23:09:06 -0500

    Initial Database Schema Setup

--- app.py
+++ app.py
@@ -3,17 +3,46 @@ from flask_cors import CORS, cross_origin
 from google.oauth2 import id_token
 from google.auth.transport.requests import Request
 import yaml
-
-
+import mysql.connector
 
 app = Flask(__name__)
-
 CORS(app)
 
+# secret credentials
 creds = yaml.safe_load(open('credentials.yaml'))
 
-@app.route('/googlelogin', methods=['POST'])
+# creating the SQL Connection
+mydb = mysql.connector.connect(
+    host = creds['DB_HOST'],
+    user = creds['DB_USER'],
+    passwd = creds['DB_PASSWORD'],
+    database = creds['DB_DATABASE'],
+)
+
+# creating the database cursor
+my_cursor = mydb.cursor()
+
+# this will be part of the user registration process
+@app.route("/register")
+def register():
+    my_cursor.execute("SELECT * from user")
+    result = my_cursor.fetchall()
+    for db in result:
+        print(db)
+    return result 
+
+# this will be part of normal user login
+@app.route("/login")
 def login():
+    my_cursor.execute("SELECT * from user")
+    result = my_cursor.fetchall()
+    for db in result:
+        print(db)
+    return result
+
+# this will be part of google login
+@app.route('/googlelogin', methods=['POST'])
+def googlelogin():
     login_data = request.json
     try:
         idinfo = id_token.verify_oauth2_token(login_data['credential'], Request(), creds['CLIENT_ID'])
@@ -33,4 +62,4 @@ def login():
     return 'success'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
