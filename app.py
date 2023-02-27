0fc2d5f552538d1e41947281d68256183f28dc94
Kamble, Nikhil <nkamble@iu.edu>
2023-02-26 21:02:25 -0500

    Login and SignUp Code Added
    Following are the two routes to use user registration and login process -
    /signup
    /login

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from google.oauth2 import id_token
 from google.auth.transport.requests import Request
 import yaml
 import mysql.connector
+import bcrypt
 
 app = Flask(__name__)
 CORS(app)
@@ -19,28 +20,89 @@ mydb = mysql.connector.connect(
     database = creds['DB_DATABASE'],
 )
 
-# creating the database cursor
-my_cursor = mydb.cursor()
+# this is the part of user registration (signup)
+@app.route("/signup", methods=['POST'])
+def signup():
+    payload = request.get_json()
+    fname = payload['firstName']
+    lname = payload['lastName']
+    email_id = payload['emailId']
+    password = str(payload['password'])
+    q1 = payload['maiden']
+    q2 = payload['artist']
+    q3 = "dummy"
 
-# this will be part of the user registration process
-@app.route("/register")
-def register():
-    my_cursor.execute("SELECT * from user")
-    result = my_cursor.fetchall()
-    for db in result:
-        print(db)
-    return result 
+    # print(fname, lname, email_id, password, password, q1, q2, q3)
 
-# this will be part of normal user login
-@app.route("/login")
+    # look for the account, if it already exists
+    my_cursor = mydb.cursor()
+    query1 = "SELECT count(*) AS count FROM user WHERE EmailID = \'" + email_id + "\';"
+    my_cursor.execute(query1)
+    records1 = my_cursor.fetchall()
+
+    if records1[0][0] == 1:
+        return jsonify({'message': 'account already exists'}), 401
+
+    # generating the salt and hashing the password with salt
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(bytes, salt)
+
+    # decoding the binary format to store them in the database
+    salt = salt.decode('utf-8')
+    hashed_password = hashed_password.decode('utf-8')
+
+    # print(salt, hashed_password)
+    
+    # counting the users in user table
+    my_cursor = mydb.cursor()
+    my_cursor.execute("SELECT count(*) AS count FROM user;")
+    records2 = my_cursor.fetchall()
+    user_no = records2[0][0] + 1
+
+    # inserting the user record
+    my_cursor = mydb.cursor()
+    query2 = "INSERT INTO user VALUES("+"\'"+str(user_no)+"\',"+"\'"+str(fname)+"\',"+"\'"+str(lname)+"\',"+"\'"+str(email_id)+"\',"+"\'"+str(salt)+"\',"+"\'"+str(hashed_password)+"\',"+"\'"+str(q1)+"\',"+"\'"+str(q2)+"\',"+"\'"+str(q3)+"\');"
+    print(query2)
+    my_cursor.execute(query2)
+    mydb.commit()
+    my_cursor.close()
+
+    return jsonify({'message': 'success'}), 200 
+
+# this will be normal user login
+@app.route("/login", methods=['POST'])
 def login():
-    my_cursor.execute("SELECT * from user")
-    result = my_cursor.fetchall()
-    for db in result:
-        print(db)
-    return result
+    payload = request.get_json()
+    email_id = str(payload['emailId'])
+    password = str(payload['password'])
 
-# this will be part of google login
+    # look for the account, if it exists or not
+    my_cursor = mydb.cursor()
+    query1 = "SELECT EmailID, Password FROM user WHERE EmailID = \'" + email_id + "\';"
+    my_cursor.execute(query1)
+    records1 = my_cursor.fetchall()
+
+    print(records1)
+
+    if len(records1) != 0:
+        # checking if the account exists or not
+        if str(records1[0][0]) == email_id:
+            pwd = records1[0][1].encode('utf-8')
+            hpwd = password.encode('utf-8')
+            res = bcrypt.checkpw(hpwd, pwd)
+            # check if the passwords match or not
+            if res:
+                return jsonify({'message': 'success'}), 200 
+            else:
+                return jsonify({'message': 'wrong password'}), 401
+        else:
+            return jsonify({'message': 'account not found'}), 401
+    else:
+        return jsonify({'message': 'account not found'}), 401
+          
+
+# this is the google login
 @app.route('/googlelogin', methods=['POST'])
 def googlelogin():
     login_data = request.json
@@ -62,4 +124,4 @@ def googlelogin():
     return 'success'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
